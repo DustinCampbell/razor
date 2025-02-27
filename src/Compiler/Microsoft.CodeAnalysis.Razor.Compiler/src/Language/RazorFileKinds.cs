65c2e5fa29381ba13eaa7d97310e71dbcf4cda5d
Dustin Campbell <dustin@teamcampbell.org>
2025-02-27 15:50:28 -0800

    Remove string-based file kind support from compiler
    - Remove RazorFileKinds members for support string-based filekinds.that are no longer needed.
    - Remove FileKinds.cs
    - Remove FileKindDirectiveFeature.cs (this appears to be an empty, unused class?)

--- src/Compiler/Microsoft.CodeAnalysis.Razor.Compiler/src/Language/RazorFileKinds.cs
+++ src/Compiler/Microsoft.CodeAnalysis.Razor.Compiler/src/Language/RazorFileKinds.cs
@@ -2,8 +2,6 @@
 // The .NET Foundation licenses this file to you under the MIT license.
 
 using System;
-using System.Collections.Frozen;
-using System.Collections.Generic;
 using System.IO;
 using Microsoft.AspNetCore.Razor.Language.Components;
 
@@ -11,37 +9,16 @@ namespace Microsoft.AspNetCore.Razor.Language;
 
 internal static class RazorFileKinds
 {
-    private static readonly FrozenDictionary<string, RazorFileKind> s_fileKindMap = new Dictionary<string, RazorFileKind>(StringComparer.OrdinalIgnoreCase)
-    {
-        [FileKinds.Component] = RazorFileKind.Component,
-        [FileKinds.ComponentImport] = RazorFileKind.ComponentImport,
-        [FileKinds.Legacy] = RazorFileKind.Legacy
-    }.ToFrozenDictionary();
-
     internal const RazorFileKind MaxValue = RazorFileKind.ComponentImport;
-
-    public static bool IsComponent(string fileKind)
-        => IsComponent(FromString(fileKind));
 
     public static bool IsComponent(RazorFileKind fileKind)
         => fileKind is RazorFileKind.Component or RazorFileKind.ComponentImport;
 
-    public static bool IsComponentImport(string fileKind)
-        => IsComponentImport(FromString(fileKind));
-
     public static bool IsComponentImport(RazorFileKind fileKind)
         => fileKind is RazorFileKind.ComponentImport;
 
-    public static bool IsLegacy(string fileKind)
-        => FromString(fileKind) is RazorFileKind.Legacy;
-
     public static bool IsLegacy(RazorFileKind fileKind)
         => fileKind is RazorFileKind.Legacy;
-
-    public static RazorFileKind FromString(string? fileKind)
-        => fileKind is not null && s_fileKindMap.TryGetValue(fileKind, out var result)
-            ? result
-            : RazorFileKind.None;
 
     public static RazorFileKind GetComponentFileKindFromFilePath(string filePath)
     {

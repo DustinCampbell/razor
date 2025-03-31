0d2fc8711e16f34c84b17f9984c75321b934fc4d
Dustin Campbell <dustin@teamcampbell.org>
2025-03-31 12:16:41 -0700

    HostDocument: Implement equality and IComparable<HostDocument>
    HostDocument is a record and should implement equality correctly. Both FilePath and TargetPath should be compared as file paths and FileKind should be compared case-insensitively (as the FileKinds class does). In addition, it should implement IComparable<HostDocument> using the same semantics.

--- src/Razor/src/Microsoft.CodeAnalysis.Razor.Workspaces/ProjectSystem/HostDocument.cs
+++ src/Razor/src/Microsoft.CodeAnalysis.Razor.Workspaces/ProjectSystem/HostDocument.cs
@@ -1,11 +1,13 @@
 ﻿// Copyright (c) .NET Foundation. All rights reserved.
 // Licensed under the MIT license. See License.txt in the project root for license information.
 
+using System;
 using Microsoft.AspNetCore.Razor.Language;
+using Microsoft.Extensions.Internal;
 
 namespace Microsoft.CodeAnalysis.Razor.ProjectSystem;
 
-internal sealed record class HostDocument
+internal sealed record class HostDocument : IComparable<HostDocument>
 {
     public RazorFileKind FileKind { get; init; }
     public string FilePath { get; init; }
@@ -16,5 +18,47 @@ internal sealed record class HostDocument
         FilePath = filePath;
         TargetPath = targetPath;
         FileKind = fileKind ?? FileKinds.GetFileKindFromPath(filePath);
+    }
+
+    public bool Equals(HostDocument? other)
+        => other is not null &&
+           FilePathComparer.Instance.Equals(FilePath, other.FilePath) &&
+           FilePathComparer.Instance.Equals(TargetPath, other.TargetPath) &&
+           StringComparer.OrdinalIgnoreCase.Equals(FileKind, other.FileKind);
+
+    public override int GetHashCode()
+    {
+        var combiner = HashCodeCombiner.Start();
+
+        combiner.Add(FilePath, FilePathComparer.Instance);
+        combiner.Add(TargetPath, FilePathComparer.Instance);
+        combiner.Add(FileKind, StringComparer.OrdinalIgnoreCase);
+
+        return combiner.CombinedHash;
+    }
+
+    public int CompareTo(HostDocument? other)
+    {
+        if (other is null)
+        {
+            return -1; // Sort null values to the end
+        }
+
+        // Compare FilePath first
+        var result = FilePathComparer.Instance.Compare(FilePath, other.FilePath);
+        if (result != 0)
+        {
+            return result;
+        }
+
+        // If FilePath is equal, compare TargetPath
+        result = FilePathComparer.Instance.Compare(TargetPath, other.TargetPath);
+        if (result != 0)
+        {
+            return result;
+        }
+
+        // If TargetPath is equal, compare FileKind
+        return StringComparer.OrdinalIgnoreCase.Compare(FileKind, other.FileKind);
     }
 }

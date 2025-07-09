52c7b64a4d48d3e565d181a27f0c66ee80a1a6e7
Dustin Campbell <dustin@teamcampbell.org>
2025-07-09 13:40:44 -0700

    Add analyzer to detect interpolated strings converted to Content
    Creating an string via interpolation and initializing `Content` with it defeats the purpose of `Content`.

---
+++ src/Analyzers/Razor.Diagnostics.Analyzers.Test/InterpolatedStringToContentAnalyzerTests.cs
@@ -0,0 +1,47 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Razor.Diagnostics.Analyzers.Test;
+
+using VerifyCS = CSharpAnalyzerVerifier<InterpolatedStringToContentAnalyzer>;
+
+public class InterpolatedStringToContentAnalyzerTests
+{
+    private const string ContentSource = """
+        namespace Microsoft.AspNetCore.Razor.Language.Intermediate
+        {
+            internal struct Content
+            {
+                public static implicit operator Content(string value)
+                    => default;
+            }
+        }
+        """;
+
+    [Fact]
+    public async Task TestLocalVariable_CSharpAsync()
+    {
+        var code = $$"""
+            using Microsoft.AspNetCore.Razor.Language.Intermediate;
+
+            class C
+            {
+                void Method()
+                {
+                    Content c = {|RZD003:$"{42} is the answer!"|};
+                    System.Console.WriteLine(c.ToString());
+                }
+            }
+
+            {{ContentSource}}
+            """;
+
+        await new VerifyCS.Test
+        {
+            TestCode = code,
+        }.RunAsync();
+    }
+}

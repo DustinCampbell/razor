6b712a865ca5f892980cf72e0c13ce7e37b99466
Dustin Campbell <dustin@teamcampbell.org>
2025-09-02 15:39:19 -0700

    Add analyzer to detect interpolated strings converted to Content
    Creating an string via interpolation and initializing `Content` with it defeats the purpose of `Content`.

--- src/Analyzers/Razor.Diagnostics.Analyzers/WellKnownTypeNames.cs
+++ src/Analyzers/Razor.Diagnostics.Analyzers/WellKnownTypeNames.cs
@@ -15,4 +15,6 @@ internal static class WellKnownTypeNames
     public const string IRemoteJsonService = "Microsoft.CodeAnalysis.Razor.Remote.IRemoteJsonService";
     public const string RazorPinnedSolutionInfoWrapper = "Microsoft.CodeAnalysis.ExternalAccess.Razor.RazorPinnedSolutionInfoWrapper";
     public const string DocumentId = "Microsoft.CodeAnalysis.DocumentId";
+
+    public const string Content = "Microsoft.AspNetCore.Razor.Language.CodeGeneration.Content";
 }
